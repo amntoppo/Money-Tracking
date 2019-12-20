01c5b1f5d24d2221a308e04890d3d7ca5b370a01
amntoppo <amntoppo@gmail.com>
2019-12-20 18:24:30 +0530

    Notification added

---
+++ app/src/main/java/com/aman/moneytracking/DetailsActivity.java
@@ -0,0 +1,4 @@
+package com.aman.moneytracking;
+
+public class DetailsActivity {
+}
--- app/src/main/java/com/aman/moneytracking/MainActivity.java
+++ app/src/main/java/com/aman/moneytracking/MainActivity.java
@@ -2,10 +2,18 @@ package com.aman.moneytracking;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.BitmapFactory;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 
@@ -19,10 +27,13 @@ public class MainActivity extends AppCompatActivity {
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECEIVE_SMS) != PackageManager.PERMISSION_GRANTED) {
            // requestPermissions(new String[]{Manifest.permission.READ_SMS}, MY_PERMISSIONS_REQUEST_READ_CONTACTS);
             ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.RECEIVE_SMS}, permissionCode);
-
-
             return;
         }
+
+        createNotificationChannel();
+        createNotification();
+
+
     }
 
 
@@ -40,5 +51,40 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         }
+    }
+
+
+    private void createNotificationChannel() {
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            NotificationChannel notificationChannel = new NotificationChannel(getString(R.string.channel_id), getString(R.string.channel_name), NotificationManager.IMPORTANCE_DEFAULT);
+            notificationChannel.setDescription(getString(R.string.channel_description));
+            notificationChannel.setShowBadge(true);
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(notificationChannel);
+            Log.e("version:", "above");
+        }
+
+    }
+
+    private void createNotification() {
+        Intent intent = new Intent(this, DetailsActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, getString(R.string.channel_id))
+                .setSmallIcon(R.drawable.small_icon)
+                .setLargeIcon(BitmapFactory.decodeResource(getResources(),R.drawable.large_icon))
+                .setContentTitle("Money Tracking")
+                .setContentText("A demo notification text")
+                .setStyle(new NotificationCompat.BigTextStyle().bigText("a demo notification text"))
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setContentIntent(pendingIntent)
+                .setChannelId(getString(R.string.channel_id))
+                .setAutoCancel(true)
+                .setOngoing(true);
+
+        Log.e("notify", "done?");
+
+        NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(this);
+        notificationManagerCompat.notify(getResources().getInteger(R.integer.notification_id), builder.build());
     }
 }
